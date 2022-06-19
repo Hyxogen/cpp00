7267367a9b1044bf037d2cf325609361fd6b7fd1
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-06-19 09:23:14 +0200

    Forgot to add header for previous commit

--- ex01/Contact.h
+++ ex01/Contact.h
@@ -1,16 +1,20 @@
 #ifndef CONTACT_H
 #define CONTACT_H
-#include <ostream>
+#include <iostream>
 #include <string>
 
-class Contact {
+struct Contact {
+    /* Usually I would make these variables private, but I really wanted to use
+     * the stream extraction operator (<<) and I wasn't allowed to use the
+     * friend keyword in this subject, so I've made it public. I did keep the
+     * underscore at the beginning to hint that this isn't part of the public
+     * interface */
     std::string _first_name;
     std::string _last_name;
     std::string _nickname;
     std::string _phone_num;
     std::string _secret;
 
-public:
     Contact(
         const std::string &first_name, const std::string &last_name,
         const std::string &nickname, const std::string &phonenum,
@@ -32,5 +36,8 @@ public:
 };
 
 std::ostream &operator<<(std::ostream &os, const Contact &contact);
+/* Like said above in contact class, I usually would make this function a friend
+ * of Contact */
+std::istream &operator>>(std::istream &is, Contact &contact);
 
 #endif
